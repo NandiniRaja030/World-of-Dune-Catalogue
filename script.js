c130a733b526b779641baa7adbd891e6fcaaf122
NandiniRaja030 <nandini.rajaradi@gmail.com>
2024-12-19 21:13:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
 //Download Alert
 function down() {
     window.alert("Your file will download.");
 }
 
-//Theme CHange Toggle
+//Theme Change Toggle
 function themeChange() {
     var thch = document.body;
     thch.classList.toggle("themeChange");
